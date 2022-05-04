2111abaef4df42d5f6c76f1fe2329ff85a59b1bc
Aleksei Zabezhinsky <aleksei.zabezhinsky@lab49.com>
2022-05-04 10:05:37 -0400

    Added ability to run Chrome engine in Docker
    Note: This doesn't work on macOS with Apple M1 chip even with `justinribeiro/chrome-headless:chromium` image

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -4,6 +4,13 @@ function fn() {
         'herokuAppTestPagesUrl': 'https://testpages.herokuapp.com/styled'
     };
 
+    if (karate.env === 'CI') {
+        karate.configure('driverTarget', {
+            docker: 'justinribeiro/chrome-headless',
+            secComp: 'src/test/java/chrome.json'
+        });
+    }
+
     // don't waste time waiting for a connection or if servers don't respond within 5 seconds
     karate.configure('connectTimeout', 5000);
     karate.configure('readTimeout', 5000);
